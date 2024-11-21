5b996d179d0a0249bbd123011037908f68ab71b7
jasmine sheikh <169073734+jasmineshamim@users.noreply.github.com>
2024-11-21 15:36:09 +0500

    Update Footer.tsx

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-[#6A1E55] text-white py-4">
+    <footer className="bg-gradient-to-r from-[#1C1C1C] to-[#7D3C98] text-white py-4">
       <div className="container mx-auto text-center">
         <p className="text-sm">
            &copy; {new Date().getFullYear()} Jasmine Sheikh. All rights reserved.
